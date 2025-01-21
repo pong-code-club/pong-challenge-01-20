8c124e5d79b7a345fd7d7bb859db6f25d1d5c5f8
CHINGZENBOOK\mondu <admin@pongcodeclub.com>
2025-01-20 21:31:43 -0500

    [keree] adding initial files

---
+++ Assets/MyCustomComponent.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MyCustomComponent : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Inside Start Function"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Inside Update Function");
+    }
+}
